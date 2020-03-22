f42cc6822480db5b4544799d1dd424c0acf05bf1
Christian Felder (masone) <ema@rh-productions.ch>
2020-03-22 13:48:16 +0100

    initial commit

---
+++ icon.jsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+
+function SvgComponent(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 268 268" fill="#fff" {...props}>
+      <path
+        d="M134 260c69.588 0 126-56.412 126-126S203.588 8 134 8 8 64.412 8 134s56.412 126 126 126z"
+        stroke="currentColor"
+        strokeWidth={0}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M134.152 179.477c24.853 0 45-20.147 45-45s-20.147-45-45-45-45 20.147-45 45 20.147 45 45 45zM120.196 90.645l-8.192-25.728M155.995 203.083l-8.191-25.727M90.644 147.804l-25.727 8.192M203.083 112.005l-25.728 8.191M154.896 93.582l12.4-23.984M100.704 198.402l12.4-23.984M93.582 113.104l-23.984-12.4M198.402 167.296l-23.984-12.4"
+        stroke="currentColor"
+        strokeWidth={15}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M114.257 126.859a6.25 6.25 0 100-12.5 6.25 6.25 0 000 12.5zM145.312 122.218a6.25 6.25 0 100-12.5 6.25 6.25 0 000 12.5zM134.314 140.412a6.25 6.25 0 100-12.5 6.25 6.25 0 000 12.5zM155.627 154.616a6.25 6.25 0 100-12.5 6.25 6.25 0 000 12.5zM120.457 159.517a6.25 6.25 0 100-12.5 6.25 6.25 0 000 12.5z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export default SvgComponent
---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+function Widget() {
+  return (
+    <>
+      <iframe src="/widget/en-us" height={56} width="100%" frameBorder={0} />
+    </>
+  )
+}
+
+export default Widget
