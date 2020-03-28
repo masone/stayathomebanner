b888c186ef0b71014669282fe064995103e6c98d
Christian Felder (masone) <ema@rh-productions.ch>
2020-03-28 20:59:01 +0100

    fix linter

--- cypress/integration/embeds.js
+++ cypress/integration/embeds.js
@@ -1,22 +1,22 @@
-context('Embeds', () => {
+context("Embeds", () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit("/");
+  });
 
   it("resizes iframes", () => {
     cy.get("#iFrameResizer0[style*=height]").then(($iframe) => {
-      expect($iframe.outerHeight()).to.be.lessThan(150)
-    })
+      expect($iframe.outerHeight()).to.be.lessThan(150);
+    });
     cy.get("#iFrameResizer1[style*=height]").then(($iframe) => {
-      expect($iframe.outerHeight()).to.be.lessThan(150)
-    })
+      expect($iframe.outerHeight()).to.be.lessThan(150);
+    });
     cy.get("#iFrameResizer2[style*=height]").then(($iframe) => {
-      console.log($iframe)
-      expect($iframe.outerHeight()).to.be.lessThan(150)
-    })
+      console.log($iframe);
+      expect($iframe.outerHeight()).to.be.lessThan(150);
+    });
     // cy.get("#iFrameResizer3[style*=height]").then(($iframe) => {
     //   console.log($iframe)
     //   expect($iframe.outerHeight()).to.be.lessThan(150)
     // })
-  })
-})
+  });
+});
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,26 @@
-const getCode = (locale, prod=true) => {
-  const host = prod ? 'https://embed.stayathomebanner.com' : 'http://localhost:3000'
+const getCode = (locale, prod = true) => {
+  const host = prod
+    ? "https://embed.stayathomebanner.com"
+    : "http://localhost:3000";
   return `
 <iframe class="stayathomebanner" src="/${locale}" width="100%" scrolling="no" frameborder="0"></iframe>
 <script src="${host}/v1.js"></script>
-  `
-}
+  `;
+};
 
 function Widget() {
   return (
-    <div style={{background: "white", color: "black", padding: "10px"}}>
+    <div style={{ background: "white", color: "black", padding: "10px" }}>
       <h2>Code</h2>
       <textarea defaultValue={getCode("de-ch")} rows="5" cols="100"></textarea>
 
       <h2>Examples</h2>
-      <div dangerouslySetInnerHTML={{__html: getCode("de-ch", false)}} />
-      <div dangerouslySetInnerHTML={{__html: getCode("en-us", false)}} />
-      <div dangerouslySetInnerHTML={{__html: getCode("de", false)}} />
-      <div dangerouslySetInnerHTML={{__html: getCode("en", false)}} />
+      <div dangerouslySetInnerHTML={{ __html: getCode("de-ch", false) }} />
+      <div dangerouslySetInnerHTML={{ __html: getCode("en-us", false) }} />
+      <div dangerouslySetInnerHTML={{ __html: getCode("de", false) }} />
+      <div dangerouslySetInnerHTML={{ __html: getCode("en", false) }} />
     </div>
-  )
+  );
 }
 
-export default Widget
+export default Widget;
