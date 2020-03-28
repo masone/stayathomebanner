91a732c8325a44b6995f57e2da3e38178d21b7df
Christian Felder (masone) <ema@rh-productions.ch>
2020-03-28 21:26:44 +0100

    dynamic host

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,28 @@
-const getCode = (locale, prod = true) => {
-  const host = prod
-    ? "https://embed.stayathomebanner.com"
-    : "http://localhost:3000";
-  return `
-<iframe class="stayathomebanner" src="/${locale}" width="100%" scrolling="no" frameborder="0"></iframe>
-<script src="${host}/v1.js"></script>
-  `;
-};
+function Widget({ host }) {
+  const getCode = (locale) => {
+    const url = (host.match("localhost") ? "http://" : "https://") + host;
+    return `
+  <iframe class="stayathomebanner" src="${url}/${locale}" width="100%" scrolling="no" frameborder="0"></iframe>
+  <script src="${url}/v1.js"></script>
+    `;
+  };
 
-function Widget() {
   return (
     <div style={{ background: "white", color: "black", padding: "10px" }}>
       <h2>Code</h2>
       <textarea defaultValue={getCode("de-ch")} rows="5" cols="100"></textarea>
 
       <h2>Examples</h2>
-      <div dangerouslySetInnerHTML={{ __html: getCode("de-ch", false) }} />
-      <div dangerouslySetInnerHTML={{ __html: getCode("en-us", false) }} />
-      <div dangerouslySetInnerHTML={{ __html: getCode("de", false) }} />
-      <div dangerouslySetInnerHTML={{ __html: getCode("en", false) }} />
+      <div dangerouslySetInnerHTML={{ __html: getCode("de-ch") }} />
+      <div dangerouslySetInnerHTML={{ __html: getCode("en-us") }} />
+      <div dangerouslySetInnerHTML={{ __html: getCode("de") }} />
+      <div dangerouslySetInnerHTML={{ __html: getCode("en") }} />
     </div>
   );
 }
+
+Widget.getInitialProps = ({ req }) => {
+  return { host: req.headers.host };
+};
 
 export default Widget;
