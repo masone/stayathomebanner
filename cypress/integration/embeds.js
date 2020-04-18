66a1e2fd7c4100de1e790108ede933b1d8edc020
Virginia <37380787+GutierrezGo@users.noreply.github.com>
2020-04-18 21:27:53 +0200

    UI-implementation (#2)

--- cypress/integration/embeds.js
+++ cypress/integration/embeds.js
@@ -10,12 +10,7 @@ context("Embeds", () => {
     cy.get("#iFrameResizer1[style*=height]").then(($iframe) => {
       expect($iframe.outerHeight()).to.be.lessThan(150);
     });
-    cy.get("#iFrameResizer2[style*=height]").then(($iframe) => {
-      console.log($iframe);
-      expect($iframe.outerHeight()).to.be.lessThan(150);
-    });
-    // cy.get("#iFrameResizer3[style*=height]").then(($iframe) => {
-    //   console.log($iframe)
+    // cy.get("#iFrameResizer2[style*=height]").then(($iframe) => {
     //   expect($iframe.outerHeight()).to.be.lessThan(150)
     // })
   });
